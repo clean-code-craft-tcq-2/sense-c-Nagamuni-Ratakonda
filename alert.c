9845501bcb177aac6f566b66265dc25dfaee6370
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2021-10-21 12:54:46 +0530

    Update alert.c
    Defined alert functions

--- alert.c
+++ alert.c
@@ -1,1 +1,20 @@
 #include "alert.h"
+
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
+{
+  if(maxThreshold < computedStats)
+  {
+    (alerters[0])();
+    (alerters[1])();
+  }
+}
+
+void EmailAlert()
+{
+  emailAlertCallCount = 1;
+}
+
+void LedAlert()
+{
+  ledAlertCallCount = 1;
+}
