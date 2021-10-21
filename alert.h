fd22739f627739798a5e341f3f3adeba8aaf6092
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2021-10-21 12:55:21 +0530

    Update alert.h

--- alert.h
+++ alert.h
@@ -1,5 +1,7 @@
 typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
+void EmailAlert();
+void LedAlert();
 
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
